0c836a88634b19491b26272eaa68c2e141b40e05
Giorgio Dal Maso <giorgio.dalmaso@develon.com>
2024-12-21 09:44:34 +0100

    cleaning

--- node_modules/@strapi/admin/admin/src/hooks/index.js
+++
@@ -1,3 +0,0 @@
-export { useContentTypes } from './useContentTypes';
-export { default as useSettingsForm } from './useSettingsForm';
-export { default as useSettingsMenu } from './useSettingsMenu';
